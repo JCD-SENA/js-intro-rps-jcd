237d9a81d644895e3108b9f22afc7a69d7dc54a8
SENA <SENA@ARMSCCCCTGOD048.FormacionSena.RED>
2024-07-19 09:13:17 -0500

    Se añade la función aleatoria del CPU

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const userRock = document.getElementById("rock")
 const userPaper = document.getElementById("paper")
 const userScissors = document.getElementById("scissors")
 
+let shiftUser = 0
+let shiftMachine = 0
+
+let countUser = 0
+let countMachine = 0
+
+const getRandomNumber = (min, max) => Math.round(Math.random() * (max - min) + min)
+
 userRock.addEventListener("click", chooseRock)
 userPaper.addEventListener("click", choosePaper)
 userScissors.addEventListener("click", chooseScissors)
@@ -17,10 +25,24 @@ function chooseRock () {
 
 function choosePaper () {
 	document.getElementById("shift-user").textContent = "El usuario eligió papel"
-	return rock
+	return scissors
 }
 
 function chooseScissors () {
 	document.getElementById("shift-user").textContent = "El usuario eligió tijera"
 	return rock
+}
+
+function chooseCpu () {
+	let machine = getRandomNumber(1,3)
+	if (machine === rock) {
+		shiftMachine = rock
+		return "La máquina eligió piedra"
+	} else if (machine === paper) {
+		shiftMachine = paper
+		return "La máquina eligió papel"
+	} else {
+		shiftMachine = scissors
+		return "La máquina eligió scissors"
+	}
 }
