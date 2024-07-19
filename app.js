b728b3060300230e56c7c51a68f8e044a79f4dd1
SENA <SENA@ARMSCCCCTGOD048.FormacionSena.RED>
2024-07-19 10:05:15 -0500

    El juego puede terminar, falta terminar el reload

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const userRock = document.getElementById("rock")
 const userPaper = document.getElementById("paper")
 const userScissors = document.getElementById("scissors")
 
+const resUser = document.getElementById("result-user")
+const resCpu = document.getElementById("result-cpu")
+
+const resultChoose = document.getElementById("result-choose")
+const shiftUserDisplay = document.getElementById("shift-user")
+
 let shiftUser = 0
 let shiftMachine = 0
 
@@ -19,18 +25,27 @@ userPaper.addEventListener("click", choosePaper)
 userScissors.addEventListener("click", chooseScissors)
 
 function chooseRock () {
-	document.getElementById("shift-user").textContent = "El usuario eligió piedra"
-	return rock
+	shiftUserDisplay.textContent = "El usuario eligió piedra"
+	shiftUser = rock
+	document.getElementById("shift-cpu").textContent = chooseCpu()
+	resultChoose.textContent = validateGame()
+	showWinner()
 }
 
 function choosePaper () {
 	document.getElementById("shift-user").textContent = "El usuario eligió papel"
-	return scissors
+	shiftUser = paper
+	document.getElementById("shift-cpu").textContent = chooseCpu()
+	resultChoose.textContent = validateGame()
+	showWinner()
 }
 
 function chooseScissors () {
 	document.getElementById("shift-user").textContent = "El usuario eligió tijera"
-	return rock
+	shiftUser = scissors
+	document.getElementById("shift-cpu").textContent = chooseCpu()
+	resultChoose.textContent = validateGame()
+	showWinner()
 }
 
 function chooseCpu () {
@@ -43,6 +58,40 @@ function chooseCpu () {
 		return "La máquina eligió papel"
 	} else {
 		shiftMachine = scissors
-		return "La máquina eligió scissors"
+		return "La máquina eligió tijeras"
 	}
+}
+
+function validateGame () {
+	if (shiftMachine == shiftUser) {
+		return "Es un empate"
+	} else if ((shiftMachine == rock && shiftUser == paper) || (shiftMachine == scissors && shiftUser == rock) || (shiftMachine == paper && shiftUser == scissors)) {
+		countUser++
+		resUser.textContent = "Usuario: "+countUser
+		return "Usuario gana"
+	} else {
+		countMachine++
+		resCpu.textContent = "CPU: "+countMachine
+		return "Ganó la máquina"
+	}
+}
+
+function showWinner() {
+	if (countMachine == 3) {
+		alert("Te ganó una IA")
+		reload()
+	} else if (countUser == 3) {
+		alert("La humanidad ganó el juego")
+		reload()
+	}
+}
+
+function reload()  {
+	let shiftUser = 0
+	let shiftMachine = 0
+	let countUser = 0
+	let countMachine = 0
+	resCpu.textContent = "CPU: "+countMachine
+	resUser.textContent = "Usuario: "+countUser
+	resultChoose.innerHTML = "&#8203;"
 }
